e57d5f7950a43cba77d5a7dcf957d9a58769ab41
Łukasz Kruszyński <s9801lk@ms.wwsi.edu.pl>
2022-11-19 12:51:06 +0100

    Usuwanie

--- FilmDB/FilmDB/Controllers/FilmController.cs
+++ FilmDB/FilmDB/Controllers/FilmController.cs
@@ -1,15 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FilmDB.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace FilmDB.Controllers
 {
     public class FilmController : Controller
     {
+        FilmManager filmManager = new FilmManager();
         public IActionResult Index()
         {
+            FilmModel film = new FilmModel();
+            film.Title = "Rambo";
+            film.Year = 1980;
+            
+            
+
+            filmManager.AddFilm(film);
+            filmManager.RemoveFilm(1005);
             return View();
         }
     }
---
+++ FilmDB/FilmDB/Logic/FilmManager.cs
@@ -0,0 +1,64 @@
+﻿using FilmDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB
+{
+    public class FilmManager
+    {
+        public FilmManager AddFilm(FilmModel filmModel)
+        {
+            using (var context = new FilmContext())
+            {
+                try
+
+                {
+                    context.Films.Add(filmModel);
+                    context.SaveChanges();
+                }
+                catch(SystemException)
+                {
+                    filmModel.ID = 0;
+                    context.Films.Add(filmModel);
+                    context.SaveChanges();
+                }
+            }
+            return this;
+        }
+
+        public FilmManager RemoveFilm(int id)
+        {
+            using (var context = new FilmContext())
+            {
+                var film = context.Films.SingleOrDefault(film=>film.ID==id);
+                context.Films.Remove(film);
+                context.SaveChanges();
+
+            }
+
+            return this;
+        }
+
+        public FilmManager UpdateFilm(FilmModel filmModel)
+        {
+            return this;
+        }
+
+        public FilmManager ChangeTitle(int id, string newTitle)
+        {
+            return this;
+        }
+
+        public FilmManager GetFilm(int id)
+        {
+            return null;
+        }
+
+        public List<FilmModel> GetFilms()
+        {
+            return null;
+        }
+    }
+}
